930fe489dc2d02c2b1934970a5e7edabc70a356d
ohagidesu <bb15121076@ms.nagasaki-u.ac.jp>
2023-08-26 01:12:18 +0900

    カリキュラム08-4. Laravel9基礎：ブログ投稿の作成

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get("/posts/{post}", [PostController::class ,"show"]);
+Route::post('/posts', [PostController::class, 'store']);
