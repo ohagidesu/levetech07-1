885fa1a416fee5af0be5c1263717804ef07b3be4
ohagidesu <bb15121076@ms.nagasaki-u.ac.jp>
2023-08-26 13:16:28 +0900

    カリキュラム08-5. Laravel9基礎：ブログ投稿の編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -17,8 +17,11 @@
                 <p>{{ $post->body }}</p>
             </div>
          </div>
+          <div class='edit'>
+             <a href="/posts/{{ $post->id }}/edit">edit</a>
+         </div>
          <div class='footer'>
-             <a herf="/">戻る</a>
+             <a href="/">戻る</a>
          </div>
     </body>
 </html>
