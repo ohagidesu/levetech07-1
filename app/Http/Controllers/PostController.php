0e4b8c4b0818086d7e728c5352d430cca3cf1695
ohagidesu <bb15121076@ms.nagasaki-u.ac.jp>
2023-08-29 15:14:04 +0900

    カリキュラム解答22. Laravel9応用：リレーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,22 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
+        //dd($post);
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
     
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
-    }
-    
-    public function create()
-    {
-    return view('posts.create');
     }
     
     public function store(PostRequest $request, Post $post)
@@ -47,6 +44,11 @@ class PostController extends Controller
     {
         $post->delete();
         return redirect('/');
+    }
+    
+    public function create(Category $category)
+    {
+        return view('posts.create')->with(['categories' => $category->get()]);
     }
 }
 ?>
